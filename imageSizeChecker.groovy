1141bceb4a564b7d4b1dc43b0e1516a6681bae0d
Mads Nielsen <man@praqma.net>
2016-11-18 14:57:19 +0100

    Fixed a possible NPE in image size checker

--- imageSizeChecker.groovy
+++ imageSizeChecker.groovy
@@ -7,22 +7,19 @@ import jenkins.model.*;
 import static groovy.io.FileType.FILES;
 
 int IMG_MAX_SIZE = 2000;
-//boolean isError = false;
+
 def workspace = "/home/jenkins/site/";
-//assert imgExtentions.any { it == ".png" }
-//println workspace.dump();
+
 new File(workspace).eachFileRecurse(FILES) {
-  //imgExtentions
-  if( it.name ==~ /([^\s]+(\.(?i)(jpg|png|gif|bmp))$)/  ){
-    println it;
+  
+  if( it.name ==~ /([^\s]+(\.(?i)(jpg|png|gif|bmp))$)/ ) {
+    println "Checking $it";
     def img = ImageIO.read(it);
-    if( img.getWidth()>IMG_MAX_SIZE || img.getHeight()>IMG_MAX_SIZE ){
+    if (img == null) {
+      println "Error: Could not process image $it"
+    } else if ( img.getWidth()>IMG_MAX_SIZE || img.getHeight()>IMG_MAX_SIZE ){
       println "Error: image ${it} size ${ img.getWidth() }x${ img.getWidth() } exceeds limit ${ IMG_MAX_SIZE }x${ IMG_MAX_SIZE }.";
-//      isError = true;
-    }
+    } else {
+      println "Image is within bounds"
   }
 }
-//if (isError == true){
-//  System.exit(1);
-//}
-//System.exit(0);
