67b5b4247858f48663fd9ebe1e0f24b53b137b75
Mads Nielsen <man@praqma.net>
2017-03-08 11:33:29 +0100

    Added a couple of more feature for #2

--- imageSizeChecker.groovy
+++ imageSizeChecker.groovy
@@ -1,13 +1,17 @@
-import java.awt.image.BufferedImage;
-import java.io.File;
-import javax.activation.MimetypesFileTypeMap;
-import javax.imageio.ImageIO;
-import jenkins.*;
-import jenkins.model.*;
-import static groovy.io.FileType.FILES;
-import groovy.transform.CompileStatic
+@GrabResolver(name='imaging', root='https://repo.adobe.com/nexus/content/repositories/public/')
+@Grapes(
+    @Grab(group='org.apache.commons', module='commons-imaging', version='1.0-R1534292')
+)
 
-def workspace = "/home/jenkins/site/";
+import java.awt.image.BufferedImage
+import java.io.File
+import javax.activation.MimetypesFileTypeMap
+import javax.imageio.ImageIO
+import jenkins.*
+import jenkins.model.*
+import static groovy.io.FileType.FILES
+
+import org.apache.commons.imaging.*
 
 public class WebsiteWarning {
   def associatedPath
@@ -28,20 +32,34 @@ public class SizeWarning extends WebsiteWarning {
 
   @Override
   public String toString() {
-    "[ImageChecker] (Warning) File ${associatedPath}. Max image size is ${maxImageSize} bytes. Was $sizeRecorded bytes"
+    "[ImageChecker] (Warning) (Filesize) File ${associatedPath}. Max image size is ${maxImageSize} kb. Was $sizeRecorded kb"
   }
 }
 
 public class UnreadableWarning extends WebsiteWarning {
-  def ex, unreadablePath
-  public UnreadableWarning(def ex, def unreadablePath) {
-    super(unreadablePath)
+  def ex, associatedPath
+
+  public UnreadableWarning(def ex, def associatedPath) {
+    super(associatedPath)
     this.ex = ex
   }
 
-  @Override
   public String toString() {
-    "[ImageChecker] (Warning) Unable to read path $path message was ${ex.message}"
+    "[ImageChecker] (Error) Unable to read path $associatedPath message was ${ex.message}"
+  }
+}
+
+public class DpiWarning extends WebsiteWarning {
+  def minDpi, recordedDpi
+
+  public DpiWarning(def minDpi, def recordedDpi, def associatedPath) {
+    super(associatedPath)
+    this.minDpi = minDpi
+    this.recordedDpi = recordedDpi
+  }
+  
+  public String toString() {
+    "[ImageChecker] (Warning) (Dpi) File ${associatedPath}. Too low DPI. Is $recordedDpi should be equal or greater than $minDpi"
   }
 }
 
@@ -56,22 +74,28 @@ class DimensionWarning extends WebsiteWarning {
     this.maxx = maxx
   }
 
-  @Override
   public String toString() {
-    "[ImageChecker] (Warning) File ${associatedPath}. Max width is ${maxy}px was ${y}px. Max height is ${maxx}px was ${x}px"
+    "[ImageChecker] (Warning) (Resolution) File ${associatedPath}. Max width is ${maxy}px was ${y}px. Max height is ${maxx}px was ${x}px"
   }    
 } 
 
 class WarningsList extends ArrayList<WebsiteWarning> {
-  
-  def imageMaxSize,maxImageWidth,maxImageHeight
+
+  def imageMaxSize,maxImageWidth,maxImageHeight,minDPI
   def scanBounds = true
   def scanSize = false
+  def scanDPI = false
 
-  public WarningsList(def imageMaxSize = 1_000_000, def maxImageWidth = 2000, def maxImageHeight = 2000) {
+  public WarningsList(def imageMaxSize = 6, def maxImageWidth = 2000, def maxImageHeight = 2000, def minDPI = 100) {
     this.maxImageHeight = maxImageHeight
     this.maxImageWidth = maxImageWidth 
     this.imageMaxSize = imageMaxSize
+    this.minDPI = minDPI
+  }
+
+  def setScanDPI(def scanDPI = true) {
+    this.scanDPI = scanDPI
+    return this
   }
 
   def setScanSize(def scanSize = true) {
@@ -87,7 +111,7 @@ class WarningsList extends ArrayList<WebsiteWarning> {
   def scan(def rootPath) {
     new File(rootPath).eachFileRecurse(FILES) {
       if( it.name ==~ /([^\s]+(\.(?i)(jpg|png|gif|bmp))$)/ ) {
-        try {
+        try {          
           if (this.scanBounds) {
             def img = ImageIO.read(it);
             if (img == null) {
@@ -96,9 +120,19 @@ class WarningsList extends ArrayList<WebsiteWarning> {
               this.add(new DimensionWarning(img.getWidth(), img.getHeight(), maxImageWidth, maxImageHeight, it))
             }
           }
+
           if (this.scanSize) {
-            if(it.length() > imageMaxSize) {
-              this.add(new SizeWarning(it, imageMaxSize, it.length()))
+            if(((int)it.length() / 1000) > imageMaxSize) {
+              this.add(new SizeWarning(it, imageMaxSize, (int)(it.length()/1024)))
+            }
+          }
+
+          if (this.scanDPI) {
+            def metadata = Imaging.getImageInfo(it)
+            if(metadata != null && metadata.getPhysicalWidthDpi() != -1 && metadata.getPhysicalHeightDpi() != -1) {
+              if(minDPI >= metadata.getPhysicalWidthDpi()) {
+                this.add(new DpiWarning(minDPI, metadata.getPhysicalWidthDpi(), it))
+              }
             }
           }
           
@@ -118,15 +152,22 @@ class WarningsList extends ArrayList<WebsiteWarning> {
   }
 }
 
-//IMPROVE-ME: This should be part of some CLI method as args
 def scanDir = System.getenv("SCAN_DIR") ?: "/home/jenkins/site/"
 def scanBounds = System.getenv("SCAN_BOUNDS") ?: false 
-def scanSize = System.getenv("SCAN_SIZE") ?: false
+def scanSize = System.getenv("SCAN_SIZE") ?: true
+def scanDPI = System.getenv("SCAN_DPI") ?: false
+def fail = System.getenv("SCAN_FAIL") ?: false
 
 println "Scan bounds: $scanBounds"
 println "Scan size: $scanSize"
 println "Scan dir: $scanDir"
+println "Scan DPI: $scanDPI" 
+println "Fail on warnings $fail" 
 
-println new WarningsList().setScanBounds(scanBounds).setScanSize(scanSize).scan(scanDir).toString()
+def warnings = new WarningsList().setScanBounds(scanBounds).setScanSize(scanSize).setScanDPI(scanDPI).scan(scanDir)
+println warnings.toString()  
 
-//TODO: Implement CLI? We can create a seperate JAR for this if need be and include it in our image. (If we want to have a totally seperate image for this) 
+if(fail) {
+  //We do NOT want to to use System.exit(...) because it can be used to shut down a VM....for example a Jenkins VM or slave service 
+  throw new RuntimeException("Warnings detected. We found ${warnings.size()} warning(s)")
+}
